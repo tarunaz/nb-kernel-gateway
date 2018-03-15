16ce8661270900d4ebd164fd3819c33310c31f33
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-15 12:29:13 +1100

    Defaults moved into notebook base image.

--- client-notebook/.jupyter/jupyter_notebook_config.py
+++ client-notebook/.jupyter/jupyter_notebook_config.py
@@ -1,6 +1,3 @@
 c.NotebookApp.session_manager_class = 'nb2kg.managers.SessionManager'
 c.NotebookApp.kernel_manager_class = 'nb2kg.managers.RemoteKernelManager'
 c.NotebookApp.kernel_spec_manager_class = 'nb2kg.managers.RemoteKernelSpecManager'
-
-import logging
-c.NotebookApp.log_level=logging.DEBUG
